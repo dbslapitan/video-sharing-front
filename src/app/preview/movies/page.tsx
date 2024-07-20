a51a4f6a4f831e5b8b83e5f8558d59f65dc19d54
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-07-20 05:41:13 -0400

    add pages for nav

---
+++ src/app/preview/movies/page.tsx
@@ -0,0 +1,5 @@
+export default function Movies(){
+    return(
+        <h1>Preview Movies</h1>
+    );
+}
---
+++ src/app/preview/tv-series/page.tsx
@@ -0,0 +1,5 @@
+export default function TVSeries(){
+    return(
+        <h1>Preview TV-Series</h1>
+    );
+}
