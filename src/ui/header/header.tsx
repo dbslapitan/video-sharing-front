9d5f735d180672c81aedf757c3933c98f86b6215
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-07-20 04:43:56 -0400

    finish header

--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import style from "./header.module.scss";
 import Navigation from "../navigation/navigation";
+import profilePic from "../../../public/image-avatar.png";
 
 export default function Header(){
+
     return (
         <header className={style["header"]}>
             <Link className={style["header__logo"]} href={'/preview'}>
             </Link>
             <Navigation/>
+            <img className={`${style["header__profile"]}`} src={profilePic.src}/>
         </header>
+
     );
 }
