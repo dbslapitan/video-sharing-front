bd48937b054f45fc5d24f78ea5319b7ab8a64ed3
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-07-21 04:44:15 -0400

    add debounce on search

--- src/ui/search/search.tsx
+++ src/ui/search/search.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { ChangeEvent } from "react";
 import style from "./search.module.scss";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search(){
 
+    const handleChange = useDebouncedCallback((event: ChangeEvent) => {
+        console.log((event.target as HTMLInputElement).value);
+    }, 400);
+
     return(
         <div className={`${style["input"]}`}>
-            <input className={`${style["input__bar"]}`} type="text" placeholder="Search for movies or TV series"/>
+            <input className={`${style["input__bar"]}`} type="text" placeholder="Search for movies or TV series" onChange={handleChange}/>
         </div>
     );
 }
