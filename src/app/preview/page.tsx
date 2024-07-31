3f3a8203d08bdb184dfa89e1063bbe60e4a92684
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-07-31 04:58:45 -0400

    add sample env

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -1,6 +1,20 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
 export default async function Preview() {
-    return(
-        <>
-        </>
-    );
+
+    const s3Client = new S3Client({region: "ca-central-1"});
+    try{
+        const url = await getSignedUrl(s3Client, new GetObjectCommand({Bucket: "video-sharing-preview", Key: '112.jpg'}));
+        console.log(url);
+
+        return(
+            <>
+                <img src={url} alt="" width={100} height={100}/>
+            </>
+        );
+    }
+    catch(e){
+        console.log(e);
+    }
 }
