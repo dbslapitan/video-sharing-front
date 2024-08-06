6b57c74e017815dcfe545d29c77de938d8b65604
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-08-06 04:30:11 -0400

    start on change handler of bookmark

--- src/ui/bookmark/bookmark.tsx
+++ src/ui/bookmark/bookmark.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { ChangeEvent } from "react";
 import style from "./bookmark.module.scss";
 
 export default function Bookmark({ isBookmarked }: {isBookmarked: boolean}){
+
+    const changeHandler = (event: ChangeEvent) => {
+        
+    }
+
     return(
-        <input className={`${style["bookmark"]}`} type="checkbox" defaultChecked={isBookmarked} />
+        <input className={`${style["bookmark"]}`} type="checkbox" defaultChecked={isBookmarked} onChange={changeHandler} />
     );
 }
